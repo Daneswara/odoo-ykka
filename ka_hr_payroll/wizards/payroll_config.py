3404836b7fe84506f819954d20bf4e682e416fe2
Hendra Juice <hd.brandoz@gmail.com>
2018-02-14 16:45:30 +0700

    add wizard settings

---
+++ ka_hr_payroll/wizards/payroll_config.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+class KaHrPayrollConfigWizard(models.TransientModel):
+    _name = 'ka_hr_payroll.config.wizard'
+
+    company_id = fields.Many2one('res.company', string="Unit/PG", required=True,
+        default=lambda self: self.env.user.company_id)
+    konjungtur = field.Float(related='company_id.konjungtur', string="Konjungtur", required=True,
+        default=1.0)
+    
+    @api.multi
+    def save_data(self):
+        self.company_id.konjungtur = self.konjungtur
