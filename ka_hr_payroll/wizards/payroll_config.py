833a8550203bd4edad943ea801d76418eaeac6aa
Cak Juice <hd.brandoz@gmail.com>
2018-02-18 15:26:27 +0700

    add docstring

--- ka_hr_payroll/wizards/payroll_config.py
+++ ka_hr_payroll/wizards/payroll_config.py
@@ -1,8 +1,18 @@
 # -*- coding: utf-8 -*-
 
+"""Author:
+	@CakJuice <hd.brandoz@gmail.com>
+
+Website:
+	https://cakjuice.com
+"""
+
 from odoo import models, fields, api
 
 class KaHrPayrollConfigWizard(models.TransientModel):
+    """Wizard to configure models which related with this module
+    """
+
     _name = 'ka_hr_payroll.config.wizard'
 
     company_id = fields.Many2one('res.company', string="Unit/PG", required=True,
@@ -12,5 +22,10 @@ class KaHrPayrollConfigWizard(models.TransientModel):
 
     @api.multi
     def save_data(self):
+        """To save data wizard. Called from button.
+
+        Decorators:
+            api.multi
+        """
         self.company_id.konjungtur_gaji = self.konjungtur_gaji
         self.company_id.konjungtur_pensiun = self.konjungtur_pensiun
